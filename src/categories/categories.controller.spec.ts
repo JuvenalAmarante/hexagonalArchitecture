299b981635cb5764909857c23c2ac37951e14445
Juvenal Amarante <juvenalamarante13@gmail.com>
2023-05-02 14:43:45 -0300

    feat: create unit test

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,20 +1,39 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { Category } from './entities/category.entity';
+import { CategoryMockRepository } from './gateways/category-mock.repository';
 
 describe('CategoriesController', () => {
-  let controller: CategoriesController;
+  let service: CategoriesService;
+  let categoryGateway: CategoryMockRepository;
+  let category: Category;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CategoriesController],
-      providers: [CategoriesService],
-    }).compile();
-
-    controller = module.get<CategoriesController>(CategoriesController);
+  beforeAll(async () => {
+    categoryGateway = new CategoryMockRepository();
+    service = new CategoriesService(categoryGateway);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Should create the record', async () => {
+    category = await service.create({ name: 'Test' });
+    expect(categoryGateway.categories).toEqual([category]);
+  });
+
+  it('Should list all records', async () => {
+    const list = await service.findAll();
+    expect(list).toEqual([category]);
+  });
+
+  it('Should list only the record', async () => {
+    const categorySearch = await service.findOne(1);
+    expect(categorySearch).toEqual(category);
+  });
+
+  it('Should update the record', async () => {
+    category = await service.update(1, { name: 'Test 2' });
+    expect(categoryGateway.categories).toEqual([category]);
+  });
+
+  it('shoul delete the record', async () => {
+    await service.remove(1);
+    expect(categoryGateway.categories).toEqual([]);
   });
 });
