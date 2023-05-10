6f6dbbfd1fe413e777e0892dd84ad0d452f01ec7
Juvenal Amarante <juvenalamarante13@gmail.com>
2023-05-10 10:42:08 -0300

    refactor(contact): update properties and messages in mock

--- src/contacts/gateways/contact-mock.repository.ts
+++ src/contacts/gateways/contact-mock.repository.ts
@@ -2,73 +2,73 @@ import { Contact } from '../entities/contact.entity';
 import { ContactGatewayInterface } from './contact-gateway-interface';
 
 export class ContactMockRepository implements ContactGatewayInterface {
-  categories: Contact[] = [];
+  contacts: Contact[] = [];
 
   async create(contact: Contact): Promise<Contact> {
-    contact.id = this.categories.length
-      ? this.categories[this.categories.length - 1].id + 1
+    contact.id = this.contacts.length
+      ? this.contacts[this.contacts.length - 1].id + 1
       : 1;
 
-    this.categories.push(contact);
+    this.contacts.push(contact);
 
     return contact;
   }
 
   async findAll(): Promise<Contact[]> {
-    return this.categories;
+    return this.contacts;
   }
 
   async findById(id: number): Promise<Contact> {
-    const contactSearch = this.categories.find((contact) => contact.id === id);
+    const contactSearch = this.contacts.find((contact) => contact.id === id);
 
     if (!contactSearch) {
-      throw new Error('Categoria não encontrada!');
+      throw new Error('Contact not found!');
     }
 
     return contactSearch;
   }
 
-  async findByCategory(id: number): Promise<Contact[]> {
-    const contactSearch = this.categories.filter(
-      (contact) => contact.id === id,
+  async findByCategory(categoryId: number): Promise<Contact[]> {
+    const contactSearch = this.contacts.filter(
+      (contact) => contact.categoryId === categoryId,
     );
 
     if (!contactSearch.length) {
-      throw new Error('Categoria não encontrada!');
+      throw new Error('Contact not found!');
     }
 
     return contactSearch;
   }
 
   async updateById(id: number, contact: Contact): Promise<Contact> {
-    const contactSearch = this.categories.findIndex(
+    const contactSearch = this.contacts.findIndex(
       (contact) => contact.id === id,
     );
 
     if (contactSearch === -1) {
-      throw new Error('Categoria não encontrada!');
+      throw new Error('Contact not found!');
     }
 
-    this.categories[contactSearch] = {
-      ...this.categories[contactSearch],
+    this.contacts[contactSearch] = {
+      ...this.contacts[contactSearch],
       ...contact,
     };
 
-    return this.categories[contactSearch];
+    return this.contacts[contactSearch];
   }
 
   async deleteById(id: number): Promise<Contact> {
-    const contactSearch = this.categories.findIndex(
+    const contactSearch = this.contacts.findIndex(
       (contact) => contact.id === id,
     );
 
     if (contactSearch === -1) {
-      throw new Error('Categoria não encontrada!');
+      throw new Error('Contact not found!');
     }
 
-    const contact = this.categories[contactSearch];
+    const contact = this.contacts[contactSearch];
 
-    this.categories.splice(contactSearch, 1);
+    this.contacts.splice(contactSearch, 1);
 
     return contact;
   }
